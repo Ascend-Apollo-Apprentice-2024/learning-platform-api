6eb8de6e0a4d823ca372091b8a9bda6c9226c555
Odigene Joseph <odigene12@gmail.com>
2024-09-11 19:18:45 -0500

    got valid json for proposals

--- LearningAPI/serializers/student_by_cohort_serializer.py
+++ LearningAPI/serializers/student_by_cohort_serializer.py
@@ -1,12 +1,27 @@
+import json
 from rest_framework import serializers
 
 
 class StudentCohortDataSerializer(serializers.Serializer):
+    proposals = serializers.SerializerMethodField()
+    
+    def get_proposals(self, obj):
+    
+     # Assuming the field storing the JSON string is called `json_field`
+        json_string = obj["proposals"]  # Replace with the actual field name
+
+        try:
+            # Parse the string into a Python list of dictionaries
+            parsed_data = json.loads(json_string)
+            print(parsed_data)
+            return parsed_data  # Return the parsed JSON data as Python objects
+        except json.JSONDecodeError:
+            return None  # Handle error gracefully, you can return an empty list or None
+    
     id = serializers.IntegerField(source='user_id')
     name = serializers.CharField(source='student_name')
     score = serializers.IntegerField()
     tags = serializers.ListField(child=serializers.CharField(), source='tag_name')
-    proposals = serializers.CharField()
     assessment_status = serializers.IntegerField(source='status_id')
     github = serializers.CharField(source='github_handle')
     archetype = serializers.CharField(source='briggs_myers_type')
